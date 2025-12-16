2f33c1d1f756daef96f6c777812b0caac588130b
Uriolivei7 <217280383+Uriolivei7@users.noreply.github.com>
2025-12-16 13:39:48 -0500

    posters 6

--- DoramasytProvider/src/main/kotlin/com/example/DoramasytProvider.kt
+++ DoramasytProvider/src/main/kotlin/com/example/DoramasytProvider.kt
@@ -30,12 +30,6 @@ class DoramasytProvider : MainAPI() {
 
         var latestCookie: Map<String, String> = emptyMap()
         var latestToken = ""
-
-        val POSTER_HEADERS = mapOf(
-            "Accept" to "image/webp,image/apng,image/*,*/*;q=0.8",
-            "Referer" to "https://www.doramasyt.com/",
-            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-        )
     }
 
     override var mainUrl = "https://www.doramasyt.com"
