fffb50dca5a0c46886ff150a707ffbdf9ea82d76
Uriolivei7 <217280383+Uriolivei7@users.noreply.github.com>
2026-04-04 12:09:23 -0500

    link 0.1.1.0

--- DoramasflixProvider/src/main/kotlin/com/example/DoramasflixExtractor.kt
+++ DoramasflixProvider/src/main/kotlin/com/example/DoramasflixExtractor.kt
@@ -7,6 +7,7 @@ import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
+import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.newExtractorLink
 
 /*
