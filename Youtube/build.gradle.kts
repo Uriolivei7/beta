3ac9cebf0f728cfd3cd991a70b449c42107e3069
Uriolivei7 <217280383+Uriolivei7@users.noreply.github.com>
2026-04-04 17:04:12 -0500

    prueba 0

---
+++ Youtube/build.gradle.kts
@@ -0,0 +1,36 @@
+@file:Suppress("UnstableApiUsage")
+
+// use an integer for version numbers
+version = 12
+
+cloudstream {
+    description = "Videos, playlists and channels from YouTube"
+    authors = listOf("Ranita")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+
+    tvTypes = listOf("Others")
+
+    requiresResources = true
+
+    /*iconUrl = "https://www.youtube.com/s/desktop/711fd789/img/logos/favicon_144x144.png"*/
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("com.github.teamnewpipe:NewPipeExtractor:v0.25.2")
+}
