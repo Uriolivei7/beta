c9d1e2575b987db71526839d6307be54eea74304
Uriolivei7 <217280383+Uriolivei7@users.noreply.github.com>
2026-05-07 10:56:42 -0500

    prueba 0

---
+++ YoutubeProvider/src/main/kotlin/com/example/youtubePlugin.kt
@@ -0,0 +1,48 @@
+package com.example
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class YoutubeTokenPlugin: Plugin() {
+    override fun load(context: Context) {
+
+        val sharedPref = context.getSharedPreferences("YouTube", Context.MODE_PRIVATE)
+
+        registerMainAPI(com.lagradost.cloudstream3.ar.youtube.YoutubeProvider(sharedPref))
+
+        openSettings = { ctx ->
+
+            val activity = ctx as? AppCompatActivity
+
+            if (activity != null) {
+
+                YoutubeSettingsBottomSheet.show(activity.supportFragmentManager, sharedPref)
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
